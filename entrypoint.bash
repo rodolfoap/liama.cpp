49eb70f3a97dae634f929244141843baff7d9257
RodolfoAP <rodolfo.alcazar@gtd.eu>
2023-05-07 12:10:43 +0200

    Changes 2023/05/07-12:10:43

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,13 +1,17 @@
 #!/bin/bash
 cd $(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 set -x
-# Simple
-# MODELREPO=eachadea/ggml-vicuna-13b-4bit
-# MODELFILE=ggml-vicuna-13b-4bit.bin
+# Quantized 4bit
+MODELREPO=eachadea/ggml-vicuna-13b-4bit
+MODELFILE=ggml-vicuna-13b-4bit.bin
 
-# Uncensored
+# Uncensored 7B
 MODELREPO=TheBloke/vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g-GGML
 MODELFILE=vicuna-AlekseyKorshuk-7B-GPTQ-4bit-128g.GGML.bin
+
+# Uncensored 13B
+MODELREPO=TheBloke/gpt4-x-vicuna-13B-GGML
+MODELFILE=gpt4-x-vicuna-13B.ggml.q4_0.bin
 
 [ -f ./models/${MODELFILE} ] || {
 	python3 getmodel.py ${MODELREPO} ${MODELFILE}
